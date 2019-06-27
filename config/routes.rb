800ea81dd1f2ded29d33514915169c4d4481ca7f
Shiv Raj <shivrajbadu@gmail.com>
2019-06-27 13:53:39 +0545

    mount Resque::Server plugins to run and see resque jobs on browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+require "resque/server"
+
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Resque::Server.new, at: "/resque"
 end
